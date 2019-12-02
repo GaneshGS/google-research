1a684da7dc5a2c08efaffb625020aebc3432321b
GaneshGS <gane.eflu@gmail.com>
2019-12-02 13:38:21 -0500

    changed 'tf.compat.v1.app.run( )'

--- albert/run_classifier.py
+++ albert/run_classifier.py
@@ -459,4 +459,4 @@ if __name__ == "__main__":
   flags.mark_flag_as_required("vocab_file")
   flags.mark_flag_as_required("albert_config_file")
   flags.mark_flag_as_required("output_dir")
-  tf.app.run()
+  tf.compat.v1.app.run()
